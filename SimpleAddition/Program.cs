30a0fca878e3def2ba9a0c46f8559633b603cb74
John Potts <john@arlingtonchurch.com>
2017-09-18 04:22:10 -0400

    MAke the program add

--- SimpleAddition/Program.cs
+++ SimpleAddition/Program.cs
@@ -10,6 +10,28 @@ namespace SimpleAddition
     {
         static void Main(string[] args)
         {
+            Console.Write("Enter the first number ");
+
+            string userInput = Console.ReadLine();
+
+            int number1 = int.Parse(userInput);
+
+            Console.Write("Enter the Second number: ");
+
+            userInput = Console.ReadLine();
+
+            int number2 = int.Parse(userInput);
+
+            int sum = number1 + number2;
+
+            Console.WriteLine("The sum is: " + sum);
+
+            Console.WriteLine("The sum is: " + sum.ToString());
+
+            Console.WriteLine("The sum is: " + (number1 + number2));
+
+            Console.ReadLine();
+
         }
     }
 }
